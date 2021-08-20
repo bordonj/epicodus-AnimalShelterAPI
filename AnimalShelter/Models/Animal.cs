b2de792c6908419523f681d0606f9dbb999dc608
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-20 14:39:46 -0700

    add models to seed data

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,13 +1,13 @@
 namespace AnimalShelter.Models
 {
-    public class Animal
-    {
-        public int AnimalId { get; set; }
-        public string Name { get; set; }
-        public string Species { get; set; }
-        public int Age { get; set; }
-        public int Size { get; set; }
-        public string Gender { get; set; }
-        public string Description { get; set; }
-    }
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public string Age { get; set; }
+    public string Size { get; set; }
+    public string Gender { get; set; }
+    public string Description { get; set; }
+  }
 }
